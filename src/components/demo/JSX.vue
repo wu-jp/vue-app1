bb1259e2ce03f1d3058f4909f3f01c551b7e70b0
wu-jp <1501453804@qq.com>
2020-07-15 17:10:29 -0700

    commit one

---
+++ src/components/demo/JSX.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'jsx',
+  components: {},
+  data() {
+   return {
+     value: 'hello JSX',
+     show: false,
+   }
+  },
+  render () {
+    return(
+      <div>
+        <h1>JSX</h1>
+        <h2>{ this.value } </h2>
+        <div domPropsInnerHTML="<a>www.baidu.com<a>"></div>
+        <div domPropsTextContent="<a>www.baidu.com<a>"></div>
+        <div v-show={this.show}>show</div>
+        { true && <div>if</div> }
+        { true ? <div>if</div> : <div>else</div> }
+      </div>
+      
+    )
+  }
+}
+</script>
